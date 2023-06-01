545b294bfff0ff4471862ff9e96a309efbed9a1d
Muhammad Shobir Abdussyakur <muhammadshobirabdussyakur@Macintoshs-MacBook-Pro.local>
2023-06-01 22:00:40 +0700

    implemented sync once.do to excute non params func exactly once

---
+++ once_test.go
@@ -0,0 +1,28 @@
+package goroutines
+
+import (
+	"fmt"
+	"sync"
+	"testing"
+)
+
+var counter = 0
+
+func OnlyOnce() {
+	counter++
+}
+
+func TestOnce(t *testing.T) {
+	var once sync.Once
+	var group sync.WaitGroup
+
+	for i := 0; i < 100; i++ {
+		go func() {
+			group.Add(1)
+			once.Do(OnlyOnce)
+			group.Done()
+		}()
+	}
+	group.Wait()
+	fmt.Println(counter)
+}
