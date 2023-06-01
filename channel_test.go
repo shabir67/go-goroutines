0942857cd697fec5bae16f9c0207ea39ccfec929
Muhammad Shobir Abdussyakur <muhammadshobirabdussyakur@Macintoshs-MacBook-Pro.local>
2023-06-01 14:14:48 +0700

    implemnted channel in and out only with channel as a parameter

--- channel_test.go
+++ channel_test.go
@@ -21,3 +21,39 @@ func TestCreateChannel(t *testing.T) {
 
 	time.Sleep(5 * time.Second)
 }
+
+func GiveMeResponse(channel chan string) {
+	time.Sleep(2 * time.Second)
+	channel <- "Prabowo SUbianto"
+
+}
+
+func TestChannnelAsParamater(t *testing.T) {
+	channel := make(chan string)
+
+	go GiveMeResponse(channel)
+
+	data := <-channel
+	fmt.Println(data)
+	defer close(channel)
+}
+
+func OnlyIn(channel chan<- string) {
+	time.Sleep(2 * time.Second)
+	channel <- "Megawati Soekarnoputri"
+}
+
+func OnlyOut(channel <-chan string) {
+	data := <-channel
+	fmt.Println(data)
+}
+
+func TestInOutChannel(t *testing.T) {
+	channel := make(chan string)
+	defer close(channel)
+
+	go OnlyIn(channel)
+	go OnlyOut(channel)
+
+	time.Sleep(5 * time.Second)
+}
