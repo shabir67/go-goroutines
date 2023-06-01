0b8de1ced40c9aef539e590c13b5e73bb0481cb5
Muhammad Shobir Abdussyakur <muhammadshobirabdussyakur@Macintoshs-MacBook-Pro.local>
2023-06-01 14:22:06 +0700

    implemented channel buffer with no receiver

--- channel_test.go
+++ channel_test.go
@@ -57,3 +57,23 @@ func TestInOutChannel(t *testing.T) {
 
 	time.Sleep(5 * time.Second)
 }
+
+func TestBufferedChannel(t *testing.T) {
+	channel := make(chan string, 3)
+	defer close(channel)
+
+	go func() {
+		channel <- "Muhammad"
+		channel <- "Shobir"
+		channel <- "Abdussyakur"
+	}()
+
+	go func() {
+
+		fmt.Println(<-channel)
+		fmt.Println(<-channel)
+		fmt.Println(<-channel)
+	}()
+	time.Sleep(2 * time.Second)
+	fmt.Println("Selesai")
+}
