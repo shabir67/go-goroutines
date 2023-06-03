f69bf5c69b351e54b95e368a8ab3ab19cde9cc73
Muhammad Shobir Abdussyakur <muhammadshobirabdussyakur@Macintoshs-MacBook-Pro.local>
2023-06-03 07:03:30 +0700

    Implemented atomic and solve race condition

---
+++ atomic_test.go
@@ -0,0 +1,27 @@
+package goroutines
+
+import (
+	"fmt"
+	"sync"
+	"sync/atomic"
+	"testing"
+)
+
+func TestAtomic(t *testing.T) {
+	var x int64 = 1
+	group := sync.WaitGroup{}
+
+	for i := 1; i < 1000; i++ {
+		go func() {
+			group.Add(1)
+			for j := 1; j < 100; j++ {
+				atomic.AddInt64(&x, 1)
+			}
+			group.Done()
+		}()
+
+	}
+	group.Wait()
+	fmt.Println("counter = ", x)
+
+}
