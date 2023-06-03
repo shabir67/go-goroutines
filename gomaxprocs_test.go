7b24410557e7aa26273989c527bee555a0ed41d4
Muhammad Shobir Abdussyakur <muhammadshobirabdussyakur@Macintoshs-MacBook-Pro.local>
2023-06-03 07:45:00 +0700

    Adding go-routines

--- gomaxprocs_test.go
+++ gomaxprocs_test.go
@@ -3,10 +3,22 @@ package goroutines
 import (
 	"fmt"
 	"runtime"
+	"sync"
 	"testing"
+	"time"
 )
 
-func TesGettGomaxprocs(t *testing.T) {
+func TestGetGomaxprocs(t *testing.T) {
+
+	group := sync.WaitGroup{}
+	for i := 0; i < 100; i++ {
+		group.Add(1)
+		go func() {
+			time.Sleep(3 * time.Second)
+			group.Done()
+		}()
+	}
+
 	totalCPU := runtime.NumCPU()
 	fmt.Println("CPU:", totalCPU)
 
